c5822e14cb3fab0c2cd456505f61be0caf03ad29
JayZhou <jay.zhou@veeva.com>
2025-02-28 17:00:02 -0500

    move home page from app/(dashboard) to app

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { ArrowRight, CreditCard, Database } from 'lucide-react';
-import { Terminal } from './terminal';
+import { Terminal } from './(dashboard)/terminal';
 
 export default function HomePage() {
   return (
